63db51236a1cc3669d97343266ad767394c3eee7
Edson Sousa <edson3711.es@gmail.com>
2023-11-19 11:16:06 -0300

    criando produto

---
+++ src/trocaBancoPostgres/migrar/Produto.java
@@ -0,0 +1,144 @@
+package trocaBancoPostgres.migrar;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import trocaBancoPostgres.conexao.ConexaoAntiga;
+import trocaBancoPostgres.conexao.ConexaoNova;
+
+public class Produto {
+
+	public static void migrarTabela() {
+		try (Connection remetenteConexao = ConexaoAntiga.obterConexao()) {
+
+			String query = "SELECT id_prod, sis_prod, ncm_prod, tipo_prod, nome_prod, edicao_prod, cfop_prod, saldo_prod, "
+					+ "valor_prod, estoque_prod, obs_prod, usu_prod, data_reg, hora_reg, valor_prod_ex, stprod, idunid\n"
+					+ "FROM public.produto " + "WHERE stprod=1 ORDER BY  sis_prod ASC;";
+
+			try (Statement sourceStatement = remetenteConexao.createStatement();
+					ResultSet resultSet = sourceStatement.executeQuery(query)) {
+
+				while (resultSet.next()) {
+					int id = resultSet.getInt("sis_prod");
+					String tipo = resultSet.getString("tipo_prod");
+					String nome = resultSet.getString("nome_prod");
+					String edicao = resultSet.getString("edicao_prod");
+					Double valor = resultSet.getDouble("valor_prod");
+					String observacao = resultSet.getString("obs_prod");
+					System.out.println("" + id);
+					salvar(id, tipo + " " + nome + " " + edicao, valor, observacao);
+				}
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void salvar(int idAntigo, String nome, Double valor, String observacao) {
+		try (Connection novaConexao = ConexaoNova.obterConexao()) {
+			String sql = "INSERT INTO produto (descricao, unidade_id, valor, ncm_id, observacao, ativo)"
+					+ " VALUES (?, ?, ?, ?, ?, true)";
+
+			try (PreparedStatement preparedStatement = novaConexao.prepareStatement(sql)) {
+				preparedStatement.setString(1, nome);
+				preparedStatement.setInt(2, 1);// unidade
+				preparedStatement.setDouble(3, valor);
+				preparedStatement.setInt(4, 1); // nvm
+				preparedStatement.setString(5, observacao);
+
+//                preparedStatement.setInt(6, idAntigo);
+
+				preparedStatement.executeUpdate();
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void dropTable(String tableName) {
+		try (Connection connection = ConexaoNova.obterConexao()) {
+			Statement statement = connection.createStatement();
+
+			String sql = "DROP TABLE " + tableName + " CASCADE";
+			statement.executeUpdate(sql);
+
+			System.out.println("Tabela " + tableName + " excluída com sucesso.");
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void createTableUnidade() {
+		try (Connection connection = ConexaoNova.obterConexao()) {
+			Statement statement = connection.createStatement();
+
+			String sql = " CREATE TABLE produto (\n" + "    id SERIAL PRIMARY KEY,\n" + "    descricao TEXT NOT NULL,\n"
+					+ "    unidade_id INT REFERENCES unidade(id),\n" + "    valor NUMERIC(10, 2) NOT NULL,\n"
+					+ "    ncm_id INT REFERENCES ncm(id),\n" + "    observacao TEXT,\n"
+					+ "    ativo BOOLEAN DEFAULT true,\n" + "    deletado BOOLEAN DEFAULT false\n" + ");";
+
+			statement.executeUpdate(sql);
+
+			System.out.println("Tabela produto criada com sucesso.");
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void adicionarColunaDeletado() {
+		try (Connection connection = ConexaoNova.obterConexao()) {
+			Statement statement = connection.createStatement();
+
+			String sql = "ALTER TABLE produto ADD deletado BOOLEAN DEFAULT false";
+			statement.executeUpdate(sql);
+
+			System.out.println("Coluna 'deletado' adicionada à tabela 'produto' com sucesso.");
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void adicionarColunaIdAntigo() {
+		try (Connection connection = ConexaoNova.obterConexao()) {
+			Statement statement = connection.createStatement();
+
+			String sql = "ALTER TABLE produto ADD idAntigo int";
+			statement.executeUpdate(sql);
+
+			System.out.println("Coluna 'idAntigo' adicionada à tabela 'produto' com sucesso.");
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void adicionarColunavaloNumerico() {
+		try (Connection connection = ConexaoNova.obterConexao()) {
+			Statement statement = connection.createStatement();
+
+			String sql = "ALTER TABLE produto\n" + "ALTER COLUMN valor TYPE NUMERIC(10,4);";
+			statement.executeUpdate(sql);
+
+			System.out.println("Coluna 'valor TYPE NUMERIC(10,4)' adicionada à tabela 'produto' com sucesso.");
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) {
+		dropTable("produto");
+		createTableUnidade();
+		adicionarColunaDeletado();
+		adicionarColunavaloNumerico();
+		adicionarColunaIdAntigo();
+		migrarTabela();
+	}
+}
